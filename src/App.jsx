9ccb7f10684bddcc8df85da5e940f09c83f37610
Zubicodes1 <97788998+Zubicodes1@users.noreply.github.com>
2023-10-07 03:59:34 +0000

    ttile style and background image changed

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
+  name: "Muhammad Zubair",
+  title: "Web developer",
+  email: "muhammadzubair12147@gmail.com",
+  gitHub: "zubicodes1",
+  instagram: "mzubair12147",
   linkedIn: "satyanadella",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "zubicodes1",
+  youTube: "zubicodes1",
 };
 
 const primaryColor = "#4E567E";
